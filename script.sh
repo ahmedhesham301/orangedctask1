886b0191a011e0481dabec25d05c7c3c204979e7
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-01-31 10:18:29 +0200

    added seperation lines

--- script.sh
+++ script.sh
@@ -13,9 +13,15 @@ done
 
 df| awk -v threshold="$diskWarningThreshold" '/^\/dev/ {print $1"\t"$5; if(($3 / $2) *100 > threshold) print "Warning: " $1 " has less than low free space"}' | tee -a "$outputFile"
 
+echo "============================================" | tee -a "$outputFile"
+
 top -bn1|awk '/%Cpu/ {print "cpu usage: " $2}' | tee -a "$outputFile"
 
+echo "============================================" | tee -a "$outputFile"
+
 free -h|awk '/Mem/ {print "used memory:" $3" ""free memory:" $4}' | tee -a "$outputFile"
+
+echo "============================================" | tee -a "$outputFile"
 
 echo "top 5 memory consuming processes" | tee -a "$outputFile"
 ps -eo pid,comm,%mem --sort=-%mem | head -n 6 | tee -a "$outputFile"
